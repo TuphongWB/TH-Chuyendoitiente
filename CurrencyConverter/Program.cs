d943597fc528d2c25b8023d5f3d89c752e3b5b22
Acer <Tuphong2611@gmail.com>
2023-05-17 08:54:53 +0700

    Add project files.

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+           
+            double rate = 23450;
+
+            Console.Write("Nhập giá trị USD: ");
+            double usdValue = Convert.ToDouble(Console.ReadLine());
+
+            double vndValue = usdValue * rate;
+            Console.WriteLine("Giá trị tương ứng của {0} USD là {1} VND", usdValue, vndValue);
+
+            Console.ReadLine();
+        }
+    }
+}
